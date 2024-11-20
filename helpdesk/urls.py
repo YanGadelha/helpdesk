59da82427d0f0c177f18f94c34565849bb29d63d
Antony-Lucas <lawokahama@gmail.com>
2024-11-20 15:08:55 -0300

    final

--- helpdesk/urls.py
+++ helpdesk/urls.py
@@ -10,4 +10,10 @@ urlpatterns = [
     path('user_profile/', views.user_profile_view, name='user_profile'),
     path('add_order/', views.add_order, name='add_order'),
     path('all-orders/', views.all_orders_view, name='all_orders'), 
+    path('user_orders/', views.user_orders_view, name='user_orders'), 
+    path('adicionar_comentario/<int:ordem_id>/', views.adicionar_comentario, name='adicionar_comentario'),
+    path('atualizar_ordem/<int:ordem_id>/', views.atualizar_ordem, name='atualizar_ordem'),
+    path('concluir_ordem/<int:ordem_id>/', views.concluir_ordem, name='concluir_ordem'),
+    path('cancelar_ordem/<int:ordem_id>/', views.cancelar_ordem, name='cancelar_ordem'),
+    path('excluir_ordem/<int:ordem_id>/', views.excluir_ordem, name='excluir_ordem'),
 ]
--- helpdesk/views.py
+++ helpdesk/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from helpdesk.models import OrdemServico
+from helpdesk.models import Comentario, OrdemServico
 from .forms import CustomUserCreationForm, LoginForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
-from django.db.models import Count
+from django.db.models import Count, Prefetch 
+from django.shortcuts import get_object_or_404, redirect
+from django.http import HttpResponseForbidden
+from django.contrib import messages
 
 User = get_user_model()
 def index_view(request):
@@ -48,14 +51,11 @@ def login_view(request):
 @login_required
 def home_view(request):
     total_ordens = OrdemServico.objects.count()
-
-    # Ordens por status
     status_counts = OrdemServico.objects.values('status').annotate(total=Count('status'))
-
-    # Inicializar contadores
     ordens_pendentes = ordens_concluidas = ordens_nao_concluidas = 0
-
-    # Atribuir os valores das contagens
+    all_orders = OrdemServico.objects.prefetch_related(
+        Prefetch('comentarios', queryset=Comentario.objects.select_related('user'))
+    )
     for status in status_counts:
         if status['status'] == 'aberto' or status['status'] == 'andamento':
             ordens_pendentes += status['total']
@@ -69,7 +69,8 @@ def home_view(request):
         'ordens_pendentes': ordens_pendentes,
         'ordens_concluidas': ordens_concluidas,
         'ordens_nao_concluidas': ordens_nao_concluidas,
-        'all_orders': OrdemServico.objects.all(),
+        'all_orders': all_orders,
+        'user_orders': OrdemServico.objects.filter(user=request.user)
     })
 
 @login_required
@@ -121,14 +122,60 @@ def add_order(request):
 
 @login_required
 def all_orders_view(request):
-    print("Entrou na view") 
     all_orders = OrdemServico.objects.all()
-    print(f"Total de ordens encontradas: {all_orders.count()}")
-    print(f"Consulta SQL: {all_orders.query}")
-    
     return render(request, 'home/home.html', {'all_orders': all_orders})
 
 @login_required
 def user_orders_view(request):
     user_orders = OrdemServico.objects.filter(user=request.user)
-    return render(request, 'home', {'user_orders': user_orders})
+    for ordem in user_orders:
+        print(f"ID: {ordem.id}, Título: {ordem.titulo}, Status: {ordem.status}, Grau de Urgência: {ordem.grau_urgencia}, Data de Criação: {ordem.data_criacao}")
+    return render(request, 'home/home.html', {'user_orders': user_orders})
+
+@login_required
+def adicionar_comentario(request, ordem_id):
+    if request.method == 'POST':
+        ordem_servico = get_object_or_404(OrdemServico, id=ordem_id)
+        comentario_texto = request.POST.get('comentario')
+
+        if not comentario_texto.strip():
+            return HttpResponseForbidden("O comentário não pode estar vazio.")
+
+        Comentario.objects.create(
+            ordem_servico=ordem_servico,
+            user=request.user,
+            comentario=comentario_texto.strip(),
+        )
+        return redirect('home')
+    
+
+def atualizar_ordem(request, ordem_id):
+    if request.method == 'POST':
+        ordem = get_object_or_404(OrdemServico, id=ordem_id, user=request.user)
+        ordem.title = request.POST.get('title')
+        ordem.description = request.POST.get('description')
+        ordem.status = request.POST.get('status')
+        ordem.urgency = request.POST.get('urgency')
+        ordem.save()
+        messages.success(request, 'Ordem atualizada com sucesso.')
+    return redirect('home')
+
+def concluir_ordem(request, ordem_id):
+    ordem = get_object_or_404(OrdemServico, id=ordem_id, user=request.user)
+    ordem.status = 'concluido'
+    ordem.save()
+    messages.success(request, 'Ordem concluída com sucesso.')
+    return redirect('home')
+
+def cancelar_ordem(request, ordem_id):
+    ordem = get_object_or_404(OrdemServico, id=ordem_id, user=request.user)
+    ordem.status = 'nao_concluido'
+    ordem.save()
+    messages.success(request, 'Ordem cancelada com sucesso.')
+    return redirect('home')
+
+def excluir_ordem(request, ordem_id):
+    ordem = get_object_or_404(OrdemServico, id=ordem_id, user=request.user)
+    ordem.delete()
+    messages.success(request, 'Ordem excluída com sucesso.')
+    return redirect('home')
